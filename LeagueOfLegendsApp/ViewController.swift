59b311432f9130fa7d9ffd8da94820b576a2b73a
Victor Moreno <victorgalves@hotmail.com>
2024-03-31 23:46:46 -0300

    Initial Commit

---
+++ LeagueOfLegendsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeagueOfLegendsApp
+//
+//  Created by admin on 31/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
